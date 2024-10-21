a4c74aa1f750c8161e892965db7144b1aff3396f
nav1211 <abhinav.vadlamani@gmail.com>
2024-10-20 22:18:46 -0400

    Adding user info database code

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask, render_template, request, redirect, url_for
+# imports
+from flask import Flask, render_template, request, redirect, url_for, flash
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
+
+# Setting up flask and app
 app = Flask(__name__)
+app.secret_key = 'blackjack.io'
 
+# Create a new client and connect to the server
+uri = "mongodb+srv://avadlamani:Breakingbad1928!@cluster0.2ohfz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Databases
+db = client['CardCountingProject']
+users_collection = db['User info']
+
+@app.route('/trainer', methods=['GET', 'POST'])
+def trainer():
+    return render_template('trainer.html')
+
+# login page
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -9,12 +28,25 @@ def login():
         username = request.form['username']
         password = request.form['password']
         
-        # placeholder for username and password logic
-        print(f"Username: {username}, Password: {password}")
+        # check if username exists
+        existing_user = users_collection.find_one({"username": username})
+
+        # when user exists
+        if existing_user:
+            correct_password = existing_user["password"]
+            if password == correct_password:
+                flash('Login Successful!')
+                return redirect(url_for('trainer'))
+            else:
+                flash('Incorrect Username/Password.')
+        else:
+            flash('Incorrect Username/Password.')
         return redirect(url_for('login'))
     
+    # render login html file
     return render_template('login.html')
 
+# register user page
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -22,9 +54,27 @@ def register():
         username = request.form['username']
         password = request.form['password']
         
-        # placeholder for usernme and password logic
-        print(f"Username: {username}, Password: {password}")
+        # checking if username already exists
+        existing_user = users_collection.find_one({"username": username})
+
+        # when user exists
+        if existing_user:
+            flash('Username already exists. Please pick another one.')
+            return redirect(url_for('register'))
+        
+        # when user doesn't already exist, add user data to 
+        else:
+            new_user = {
+                "username" : username,
+                "password" : password
+            }
+            users_collection.insert_one(new_user)
+            flash("Registration successful")
+        
+        # after registering redirect users to the login file
         return redirect(url_for('login'))
+    
+    # render register html file
     return render_template('register.html')
 
 if __name__ == '__main__':
