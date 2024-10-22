9ec6a4e7a8d140246df3ebfdeefc367a33263500
nav1211 <abhinav.vadlamani@gmail.com>
2024-10-22 15:46:13 -0400

    Added Ace Logic

--- counting_logic.py
+++ counting_logic.py
@@ -1,7 +1,7 @@
 import random
 
 # Creating set of cards
-ranks = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'}
+ranks = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'}
 suits = {'Hearts', 'Diamonds', 'Clubs', 'Spades'}
 deck_of_cards = {f"{rank} of {suit}" for rank in ranks for suit in suits}
 
@@ -11,7 +11,7 @@ count = 0
 # Sets of cards based on count
 positive_count = {'2', '3', '4', '5', '6'}
 neutral_count = {'7', '8', '9'}
-negative_count = {'10', 'Jack', 'Queen', 'King'}
+negative_count = {'10', 'Jack', 'Queen', 'King', 'Ace'}
 positive_count_cards = {f"{rank} of {suit}" for rank in positive_count for suit in suits}
 neutral_count_cards = {f"{rank} of {suit}" for rank in neutral_count for suit in suits}
 negative_count_cards = {f"{rank} of {suit}" for rank in negative_count for suit in suits}
@@ -28,8 +28,6 @@ for card in deck_of_cards:
         card_values[card] = int(rank)
     elif rank in ["Jack", "Queen", "King"]:
         card_values[card] = 10
-    else:
-        print(card)
 
 # helper function to update count
 def update_count(card):
@@ -53,16 +51,65 @@ def start_play():
     # Player first card
     player_first_card = deal()
     # Dealer first card
-    dealer_second_card = deal()
+    dealer_first_card = deal()
     # Player second card
     player_second_card = deal()
     # Dealer second card
     dealer_second_card = deal()
     
-    return (player_first_card, dealer_second_card, player_second_card, dealer_second_card)
+    return (player_first_card, dealer_first_card, player_second_card, dealer_second_card)
+
+def count_aces(set_of_cards):
+    aces = 0
+    for card in set_of_cards:
+        if "Ace" in card:
+            aces += 1
+    return aces
+
+def max(val1, val2):
+    if val1 > 21:
+        return val2
+    elif val2 > 21:
+        return val1
+    elif val1 == 21:
+        return val1
+    elif val2 == 21:
+        return val2
+    elif val1 > val2:
+        return val1
+    else:
+       return val2
+
+def calculate_value(set_of_cards):
+    aces = count_aces(set_of_cards)
+    
+    # compute values for non aces
+    value = 0
+    for card in set_of_cards:
+        if not("Ace" in card):
+            value += card_values[card]
+    # Compute for one ace
+    if aces == 1:
+        return max(value + 1, value + 11)
+    
+    # Compute for two aces
+    if aces == 2:
+        return max(value + 2, value + 12)
+    
+    # Compute for three aces
+    if aces == 3:
+        return max(value + 3, value + 13)
+    
+    # Compute for four aces
+    if aces == 4:
+        return max(value + 4, value + 14)
+    
+    # Compute for no aces
+    else:
+        return value
 
 # play two rounds
-for i in range(2):
+while True:
     (player_first_card, dealer_first_card, player_second_card, dealer_second_card) = start_play()
 
     # Make sets for Ace total sum logic
@@ -71,7 +118,7 @@ for i in range(2):
     player_cards.add(player_first_card); player_cards.add(player_second_card)
     dealer_cards.add(dealer_first_card); dealer_cards.add(dealer_second_card)
     
-    player_value = card_values[player_first_card] + card_values[player_second_card]
+    player_value = calculate_value(player_cards)
 
     # Give player information
     print(f"Dealer first card: {dealer_first_card}")
@@ -89,8 +136,7 @@ for i in range(2):
             hit_card = deal()
             print(f"Hit Card: {hit_card}")
             player_cards.add(hit_card)
-            player_value += card_values[hit_card]
-
+            player_value = calculate_value(player_cards)
             # Check if player busts
             if player_value > 21:
                 print("You Bust!")
@@ -102,20 +148,27 @@ for i in range(2):
                 player_hit = False
         # If player doesn't want to hit, break out of the loop
         else:
+            if player_value == 21:
+                print("You Win!")
             player_hit = False
 
     # If player busts, move on to next round
     if bust:
         print(count)
-        continue
+        play = input("Would you like to keep playing (Yes or No): ")
+        if play == "No":
+            break
+        else:
+            continue
 
     # Run through dealer play
     print(f"Dealer second card: {dealer_second_card}")
-    dealer_value = card_values[dealer_first_card] + card_values[dealer_second_card]
+    dealer_value = calculate_value(dealer_cards)
     while dealer_value < 17: 
         dealer_hit_card = deal()
         print(f"Dealer hit card: {dealer_hit_card}")
-        dealer_value += card_values[dealer_hit_card]
+        dealer_cards.add(dealer_hit_card)
+        dealer_value = calculate_value(dealer_cards)
     
     # Check conditions for if player wins or not
     if dealer_value == 21:
@@ -127,6 +180,10 @@ for i in range(2):
     else:
         print("You Win!")
     
+    play = input("Would you like to keep playing (Yes or No): ")
+    print(count)
+    if play == "No":
+        break
 
             
 
